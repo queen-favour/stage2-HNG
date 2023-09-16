f846f1fe80be548ae47657a7118061d2b2b94b79
queen-favour <elechifavour3@gmail.com>
2023-09-16 14:37:30 +0100

    Added browerRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // App.js
 
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import MovieDetails from "../src/components/MovieDetail";
 
 function App() {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/" exact element={<Home />} />
       <Route path="/movies/:id" element={<MovieDetails />} />
     </Routes>
+    </BrowserRouter>
   );
 }
 
