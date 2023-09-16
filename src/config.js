0aaa51c4d7a31d2071da2ca8f58b22b186b145ba
queen-favour <elechifavour3@gmail.com>
2023-09-16 18:51:42 +0100

    corrected the ApiUrl for the Top rated movies

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  moviedbApiUrl: 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=',
+  // moviedbApiUrl: 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=',
+  moviedbApiUrl: 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
   moviedbAuthToken: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTcyN2VmZjU3MjRhM2Q3ZjM3YjBiMGRhY2EwN2I2MiIsInN1YiI6IjY0ZmVjZmYxNmEyMjI3MDBjM2I1MzM0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xSNA6RDhbNpE_I90ngPddOpK5ccz-TqeG_Kq6y7ZGsk',
 };
 
